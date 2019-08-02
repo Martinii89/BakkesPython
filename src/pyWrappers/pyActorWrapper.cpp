ac5bd341e3b9ef999dacf8c64258bdd79d2145c2
Martin VN <m4rtini89@gmail.com>
2019-08-02 12:28:13 +0200

    Forgot one method..

--- src/pyWrappers/pyActorWrapper.cpp
+++ src/pyWrappers/pyActorWrapper.cpp
@@ -5,9 +5,10 @@ void init_actorwrapper(py::module& m) {
 	py::class_<ActorWrapper, std::shared_ptr<ActorWrapper>>(m, "ActorWrapper")
 		.def("GetLocation", &ActorWrapper::GetLocation)
 		.def("SetLocation", &ActorWrapper::SetLocation)
+		.def("GetVelocity", &ActorWrapper::GetVelocity)
 		.def("SetVelocity", &ActorWrapper::SetVelocity)
 		.def("AddVelocity", &ActorWrapper::AddVelocity)
-		.def("GetRotation", &ActorWrapper::GetRotation)
+		//.def("GetRotation", &ActorWrapper::GetRotation) TODO: Wrap Rotator 
 		.def("SetRotation", &ActorWrapper::SetRotation)
 		.def("Stop", &ActorWrapper::Stop)
 		.def("GetAngularVelocity", &ActorWrapper::GetAngularVelocity)
