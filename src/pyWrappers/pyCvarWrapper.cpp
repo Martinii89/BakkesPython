fa9aa764657d9cc1c215906fb1997319a4a5cbcf
Martin VN <m4rtini89@gmail.com>
2019-08-04 12:30:19 +0200

    Removing manual binding.

--- src/pyWrappers/pyCvarWrapper.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "bakkesmod/wrappers/cvarmanagerwrapper.h"
-
-void init_cvarmanager(py::module& m) {
-	py::class_<CVarManagerWrapper, std::shared_ptr<CVarManagerWrapper>>(m, "CVarManagerWrapper")
-		.def("log", &CVarManagerWrapper::log, "Log message to console", py::arg("msg"));
-}
